2740584bd7039582738a8c2caa755de53721816d
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2014-11-19 21:00:10 -0500

    Queened

--- JSver/app/js/Board.js
+++ JSver/app/js/Board.js
@@ -56,6 +56,10 @@ var Board = function() {
       grid[1][col] = new Rook(1, 1, col);
     }
 
+    //Place the queens
+    grid[8][5] = new Queen(0, 8, 5)
+    grid[1][4] = new Queen(1, 1, 4);
+
     this.updateMoves();
   }
 
--- JSver/app/js/Queen.js
+++ JSver/app/js/Queen.js
@@ -1,4 +1,4 @@
-function Queen (playerColour, row, col){
+function Queen(playerColour, row, col) {
   var score = 8;
   Piece.call(this, score, playerColour, row, col);
 }
@@ -9,14 +9,20 @@ Queen.prototype.getName = function() {
   return 'Queen';
 }
 
-Queen.prototype.getAllPossibleMoves = function(){
+Queen.prototype.getAllPossibleMoves = function() {
   board = new Board();
-  for (var dy = -1; dy <= 1; dy++){
-    for (var dx = -1; dx <= 1; dx++)
-      {
-        dRow = this.row + fy;
-        dCol = this.row + fx;
-        
+  allPossibleMoves = []
+  for (var dy = -1; dy <= 1; dy++) {
+    for (var dx = -1; dx <= 1; dx++) {
+      dRow = this.row + dy;
+      dCol = this.row + dx;
+      while (dRow < 9 && dRow > 0 && dCol < 9 && dCol > 0 && board.getPiece(
+          dRow, dCol) == 0) {
+        allPossibleMoves.push(new Move(this.row, this.col, dRow, dCol));
+        dRow += dy;
+        dCol += dx;
       }
+    }
   }
+  this.allPossibleMoves = allPossibleMoves;
 }
