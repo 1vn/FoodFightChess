9f475902e81446207e0295bf7573a0e1593eec0f
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2014-12-02 04:11:28 -0500

    started bishop

--- JSver/app/js/Bishop.js
+++ JSver/app/js/Bishop.js
@@ -1,3 +1,16 @@
-var Bishop = function(playerColour, row, col){
+var Bishop = function(playerColour, row, col) {
+  var score = 5;
+  Piece.call(this, score, playerColour, row, col);
+}
 
+Bishop.prototype = Object.create(Piece.prototype);
+Bishop.prototype.constructor = Bishop;
+Bishop.prototype.copy = function() {
+  copy = new Bishop(this.playerColour, this.row, this.col);
+  copy.removeDiv();
+  return copy;
+}
+
+Bishop.prototype.getName = function() {
+  return "Bishop"
 }
