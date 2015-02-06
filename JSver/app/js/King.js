7fdb169c751c70d5f09f5a2ec6e2fa62621e8689
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-02-06 18:47:06 -0500

    adjusting move achitecture

--- JSver/app/js/King.js
+++ JSver/app/js/King.js
@@ -1,30 +1,46 @@
 var King = function(playerColour, row, col) {
-	var score = 200;
-	Piece.call(this, score, playerColour, row, col)
+    var score = 200;
+    Piece.call(this, score, playerColour, row, col)
 }
 King.prototype = Object.create(Piece.prototype);
 King.prototype.constructor = King;
 King.prototype.getName = function() {
-	return 'King';
+    return 'King';
 }
 
 
 King.prototype.copy = function() {
-	copy = new King(this.colour, this.row, this.col)
-	return copy;
+    copy = new King(this.colour, this.row, this.col)
+    return copy;
 }
 
 King.prototype.updateKingPos = function() {
-	return [this.row, this.col]
+    return [this.row, this.col]
 }
 
 King.prototype.getAllPossibleMoves = function() {
-	allPossibleMoves = []
-	board = new Board();
-	for (var dx = -1; dx <= 1; dx++) {
-		for (var dy = -1; dy <= 1; dy++) {
-
-		}
-	}
-
+    allPossibleMoves = []
+    board = new Board();
+    for (var dx = -1; dx <= 1; dx++) {
+        dCol = this.col + dx
+        for (var dy = -1; dy <= 1; dy++) {
+            dRow = this.row + dy
+            if ((board.getPiece(dRow, dCol) != 0 &&
+                    board.getPiece(dRow, dCol).getColour() != this.colour) ||
+                board.getPiece(dRow, dCol) == 0) {
+                move = new Move(this.row, this.col, dRow, dCol)
+                allPossibleMoves.push(move)
+                var toSpace = board.getPiece(dRow, dCol)
+                if (this.colour == 0 && Piece.prototype.bTargets.indexOf(
+                        toSpace) <=
+                    -1)
+                    Piece.prototype.bTargets.push(toSpace);
+                else if (this.colour == 1 && Piece.prototype.wTargets.indexOf(
+                        toSpace) <=
+                    -1)
+                    Piece.prototype.wTargets.push(toSpace);
+            }
+        }
+    }
+    this.allPossibleMoves = allPossibleMoves
 }
--- JSver/app/js/Move.js
+++ JSver/app/js/Move.js
@@ -1,13 +1,18 @@
 var Move = function(fromR, fromC, toR, toC) {
-  this.fromRow = fromR;
-  this.fromCol = fromC;
-  this.toRow = toR;
-  this.toCol = toC;
-  this.enPassant = false;
-  this.promote = false;
-  Move.prototype.getInfo = function() {
-    info = "fromRow: " + this.fromRow + " fromCol: " + this.fromCol +
-      " toRow: " + this.toRow + " toCol: " + this.toCol;
-    return info;
-  }
+	this.fromRow = fromR;
+	this.fromCol = fromC;
+	this.toRow = toR;
+	this.toCol = toC;
+	this.enPassant = false;
+	this.promote = false;
+	Move.prototype.getInfo = function() {
+		info = "fromRow: " + this.fromRow + " fromCol: " + this.fromCol +
+			" toRow: " + this.toRow + " toCol: " + this.toCol;
+		return info;
+	}
+
+	Move.prototype.equals = function(other) {
+		return this.fromRow == other.fromRow && this.fromCol == other.fromCol &&
+			this.toRow == other.toRow && this.toCol == other.toCol
+	}
 }
