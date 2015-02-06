d9586bd80721113afbf41cf9cb6f6a734346b265
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-02-06 17:43:27 -0500

    initial king

--- JSver/app/js/Board.js
+++ JSver/app/js/Board.js
@@ -1,172 +1,176 @@
 var Board = function() {
-    var SQUARE_DIM = 80;
-    var NO_ROWS = 10;
-    var NO_COLS = 10;
-    var instance = null;
-    var test;
-    img = new Image();
-    var drawn;
-    var grid = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ];
-    if (arguments.callee._singletonInstance) //Singleton block
-        return arguments.callee._singletonInstance;
-    arguments.callee._singletonInstance = this;
+	var SQUARE_DIM = 80;
+	var NO_ROWS = 10;
+	var NO_COLS = 10;
+	var instance = null;
+	var test;
+	img = new Image();
+	var drawn;
+	var grid = [
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	];
+	if (arguments.callee._singletonInstance) //Singleton block
+		return arguments.callee._singletonInstance;
+	arguments.callee._singletonInstance = this;
 
-    this.draw = function() {
-        console.log("board drawn");
-        var canvas = document.getElementById("canvas");
-        var context = canvas.getContext("2d");
-        for (var row = 1; row < NO_ROWS - 1; row++) {
-            for (var col = 1; col < NO_COLS - 1; col++) {
-                var xPos = (col - 1) * SQUARE_DIM;
-                var yPos = (row - 1) * SQUARE_DIM;
-                if ((row % 2 == 0 && col % 2 == 0) || (row % 2 != 0 &&
-                        col % 2 != 0)) {
-                    context.fillStyle = 'black';
-                    context.fillRect(xPos, yPos, SQUARE_DIM, SQUARE_DIM);
-                } else {
-                    context.fillStyle = 'white';
-                    context.fillRect(xPos, yPos, SQUARE_DIM, SQUARE_DIM);
-                }
-            }
-        }
-        drawn = true;
-    }
+	this.draw = function() {
+		console.log("board drawn");
+		var canvas = document.getElementById("canvas");
+		var context = canvas.getContext("2d");
+		for (var row = 1; row < NO_ROWS - 1; row++) {
+			for (var col = 1; col < NO_COLS - 1; col++) {
+				var xPos = (col - 1) * SQUARE_DIM;
+				var yPos = (row - 1) * SQUARE_DIM;
+				if ((row % 2 == 0 && col % 2 == 0) || (row % 2 != 0 &&
+						col % 2 != 0)) {
+					context.fillStyle = 'black';
+					context.fillRect(xPos, yPos, SQUARE_DIM, SQUARE_DIM);
+				} else {
+					context.fillStyle = 'white';
+					context.fillRect(xPos, yPos, SQUARE_DIM, SQUARE_DIM);
+				}
+			}
+		}
+		drawn = true;
+	}
 
-    this.setUpBoard = function() {
+	this.setUpBoard = function() {
 
-        //Place the pawns
-        for (var col = 1; col < NO_COLS - 1; col++) {
-            grid[7][col] = new Pawn(0, 7, col);
-            grid[2][col] = new Pawn(1, 2, col);
-        }
+		//Place the pawns
+		for (var col = 1; col < NO_COLS - 1; col++) {
+			grid[7][col] = new Pawn(0, 7, col);
+			grid[2][col] = new Pawn(1, 2, col);
+		}
 
-        //Place the rooks
-        for (var col = 1; col < NO_COLS - 1; col += 7) {
-            grid[8][col] = new Rook(0, 8, col);
-            grid[1][col] = new Rook(1, 1, col);
-        }
+		//Place the rooks
+		for (var col = 1; col < NO_COLS - 1; col += 7) {
+			grid[8][col] = new Rook(0, 8, col);
+			grid[1][col] = new Rook(1, 1, col);
+		}
 
-        //Place knights
-        for (var col = 2; col < NO_COLS - 1; col += 5) {
-            grid[8][col] = new Knight(0, 8, col);
-            grid[1][col] = new Knight(1, 1, col);
-        }
+		//Place knights
+		for (var col = 2; col < NO_COLS - 1; col += 5) {
+			grid[8][col] = new Knight(0, 8, col);
+			grid[1][col] = new Knight(1, 1, col);
+		}
 
-        //Place Bishops
-        for (var col = 3; col < 7; col += 3) {
-            grid[8][col] = new Bishop(0, 8, col);
-            grid[1][col] = new Bishop(1, 1, col);
-        }
+		//Place Bishops
+		for (var col = 3; col < 7; col += 3) {
+			grid[8][col] = new Bishop(0, 8, col);
+			grid[1][col] = new Bishop(1, 1, col);
+		}
 
-        //Place the queens
-        grid[8][5] = new Queen(0, 8, 5);
-        grid[1][4] = new Queen(1, 1, 4);
+		//Place the queens
+		grid[8][5] = new Queen(0, 8, 5);
+		grid[1][4] = new Queen(1, 1, 4);
 
-        this.updateMoves();
-    }
+		//Place the kings
+		grid[8][4] = new King(0, 8, 4);
+		grid[1][5] = new King(1, 1, 4);
 
-    this.tester = function(num) {
-        test = num;
-        return test;
-    }
+		this.updateMoves();
+	}
 
-    this.getBoard = function() {
-        return grid;
-    }
+	this.tester = function(num) {
+		test = num;
+		return test;
+	}
 
-    this.remove = function(row, col) {
-        grid[row][col] = 0;
-    }
+	this.getBoard = function() {
+		return grid;
+	}
 
-    this.getPiece = function(row, col) {
-        if (grid[row][col] != 0) {
-            return grid[row][col];
-        } else if (grid[row][col] == 0) {
-            return grid[row][col];
-        }
-    }
+	this.remove = function(row, col) {
+		grid[row][col] = 0;
+	}
 
-    this.move = function(move, piece) {
-        if (piece.isValidMove(move)) {
-            grid[move.fromRow][move.fromCol].move(move);
-            grid[move.toRow][move.toCol] = piece;
-            grid[move.fromRow][move.fromCol] = 0;
-        } else {
-            grid[move.fromRow][move.fromCol].noMove();
-        }
-        //this.remember();
+	this.getPiece = function(row, col) {
+		if (grid[row][col] != 0) {
+			return grid[row][col];
+		} else if (grid[row][col] == 0) {
+			return grid[row][col];
+		}
+	}
 
-    }
+	this.move = function(move, piece) {
+		if (piece.isValidMove(move)) {
+			grid[move.fromRow][move.fromCol].move(move);
+			grid[move.toRow][move.toCol] = piece;
+			grid[move.fromRow][move.fromCol] = 0;
+		} else {
+			grid[move.fromRow][move.fromCol].noMove();
+		}
+		//this.remember();
 
-    /**
-     *Precondition: move is a valid move
-     *
-     */
-    this.forceMove = function(move, piece) {
-        grid[move.fromRow][move.fromCol].move(move);
-        grid[move.toRow][move.toCol] = piece;
-        grid[move.fromRow][move.fromCol] = 0;
-    }
+	}
 
-    this.getScore = function(colour) {
-        board = new Board(); //getInstance
-        score = 0
-        for (var row = 1; row < 9; row++) {
-            for (var col = 1; col < 9; col++) {
-                space = board.getPiece(row, col);
-                if (space != 0 && space.getColour != colour)
-                    score += space.getScore();
-            }
-        }
-    }
+	/**
+	 *Precondition: move is a valid move
+	 *
+	 */
+	this.forceMove = function(move, piece) {
+		grid[move.fromRow][move.fromCol].move(move);
+		grid[move.toRow][move.toCol] = piece;
+		grid[move.fromRow][move.fromCol] = 0;
+	}
 
-    this.updateMoves = function() {
-        for (row = 1; row < 9; row++) {
-            for (col = 1; col < 9; col++) {
-                if (grid[row][col] != 0) {
-                    grid[row][col].getAllPossibleMoves();
-                    console.log("Updated moves for: " + grid[row][col].getName())
-                }
-            }
-        }
-    }
+	this.getScore = function(colour) {
+		board = new Board(); //getInstance
+		score = 0
+		for (var row = 1; row < 9; row++) {
+			for (var col = 1; col < 9; col++) {
+				space = board.getPiece(row, col);
+				if (space != 0 && space.getColour != colour)
+					score += space.getScore();
+			}
+		}
+	}
 
-    /**
-      Store the last state of the board
-    */
-    this.remember = function() {
-        mStack = new MentoStack();
-        mento = new MentoBoard();
-        console.log("mento created");
-        mStack.push(mento);
-    }
+	this.updateMoves = function() {
+		for (row = 1; row < 9; row++) {
+			for (col = 1; col < 9; col++) {
+				if (grid[row][col] != 0) {
+					grid[row][col].getAllPossibleMoves();
+					console.log("Updated moves for: " + grid[row][col].getName())
+				}
+			}
+		}
+	}
 
-    /**
-      Gets last state of the board
-    */
-    this.undo = function(step) {
-        mStack = new MentoStack();
-        var s = step
-        while (s > 1)
-            mStack.pop();
-        var mento = mStack.pop();
-        for (var row = 1; row < 9; row++)
-            for (var col = 1; col < 9; col++) {
-                space = mento.getPiece(row, col)
-                if (space != 0) {
-                    grid[row][col] = space.copy()
-                }
-            }
-    }
+	/**
+	  Store the last state of the board
+	*/
+	this.remember = function() {
+		mStack = new MentoStack();
+		mento = new MentoBoard();
+		console.log("mento created");
+		mStack.push(mento);
+	}
+
+	/**
+	  Gets last state of the board
+	*/
+	this.undo = function(step) {
+		mStack = new MentoStack();
+		var s = step
+		while (s > 1)
+			mStack.pop();
+		var mento = mStack.pop();
+		for (var row = 1; row < 9; row++)
+			for (var col = 1; col < 9; col++) {
+				space = mento.getPiece(row, col)
+				if (space != 0) {
+					grid[row][col] = space.copy()
+				}
+			}
+	}
 }
--- JSver/app/js/King.js
+++ JSver/app/js/King.js
@@ -1,19 +1,30 @@
 var King = function(playerColour, row, col) {
-  var score = 200;
-  Piece.call(this, score, playerColour, row, col)
+	var score = 200;
+	Piece.call(this, score, playerColour, row, col)
 }
 King.prototype = Object.create(Piece.prototype);
 King.prototype.constructor = King;
 King.prototype.getName = function() {
-  return 'King';
+	return 'King';
 }
 
 
-King.protoype.copy = function() {
-  copy = new King(this.colour, this.row, this.col)
-  return copy;
+King.prototype.copy = function() {
+	copy = new King(this.colour, this.row, this.col)
+	return copy;
 }
 
 King.prototype.updateKingPos = function() {
-  return [this.row, this.col]
+	return [this.row, this.col]
+}
+
+King.prototype.getAllPossibleMoves = function() {
+	allPossibleMoves = []
+	board = new Board();
+	for (var dx = -1; dx <= 1; dx++) {
+		for (var dy = -1; dy <= 1; dy++) {
+
+		}
+	}
+
 }
